45d0ad5eb0384ae3bc068c2da05e856ab1425074
saitoyu1314 <147906474+saitoyu1314@users.noreply.github.com>
2024-05-24 18:10:04 +0900

    ベルトコンベア作成

--- Assets/script/pearl.cs
+++ Assets/script/pearl.cs
@@ -11,12 +11,19 @@ public class pearl : MonoBehaviour
     public Transform pearl_limit;
     public List<GameObject> _pearlist = new List<GameObject>();
     private GameObject _pearlprefab;
+    private float rnd;
 
     private void Start()
     {
         //真珠の生成
         _pearlprefab = Resources.Load<GameObject>("pearl");
-        InvokeRepeating(nameof(Production), 0.0f, 1.0f);
+        InvokeRepeating(nameof(Production), 0.0f, 5.0f);
+        
+    }
+
+    private void Update()
+    {
+        rnd = Random.Range(-1.0f, 1.0f);
     }
 
     private void Production()
@@ -26,20 +33,22 @@ public class pearl : MonoBehaviour
         {
             var obj = Instantiate(_pearlprefab,Vector3.zero,Quaternion.identity, pearl_limit);
             obj.GetComponent<pearl_counter>().Setup(DeleteObject);
-            if (_pearlist.Count >= 3)
+            obj.transform.position = new Vector3(-15f, 5.0f, rnd);
+
+            /* if (_pearlist.Count >= 3)
             {
                 //3個生成したら座標を横にずらす
-                obj.transform.position = new Vector3(1.5f, 5.0f, 0.0f);
+                obj.transform.position = new Vector3(-8f, 5.0f, 0.0f);
             }
             else
             {
-                obj.transform.position = Vector3.up * 5;
-            }
+                obj.transform.position = new Vector3(-8f, 5.0f, 0.0f);
+            }*/
             _pearlist.Add(obj);
             Debug.Log("生成");
         }
     }
-
+    //真珠の要素数を削除
     private void DeleteObject(GameObject obj)
     {
         _pearlist.Remove(obj);
