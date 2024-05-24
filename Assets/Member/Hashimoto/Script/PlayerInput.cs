3ffe27f1d04ecdd3140e0eec72f5ea326f29971c
ohashirasure <TTC2350DG0008@edu.tech.ac.jp>
2024-05-24 14:15:52 +0900

    プレイヤーの操作

--- Assets/Member/Hashimoto/Script/PlayerInput.cs
+++ Assets/Member/Hashimoto/Script/PlayerInput.cs
@@ -1,11 +1,20 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using UnityEditorInternal;
 
 public class PlayerInput : MonoBehaviour
 {
     private ActionControl _actionControl;
-    // Start is called before the first frame update
+
+    private Vector3 BefoPos;
+
+    private Rigidbody rb;
+    private float upForce;
+    private bool Jumping = false;
 
     private void Awake()
     {
@@ -13,10 +22,40 @@ public class PlayerInput : MonoBehaviour
         _actionControl.Enable();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        upForce = 250;
+        rb = GetComponent<Rigidbody>();//ジャンプ
+    }
+
     void Update()
     {
         var pos = _actionControl.Player.Move.ReadValue<Vector2>();
-        transform.localPosition += new Vector3(pos.x, 0, pos.y) * 0.05f;
+        transform.position += new Vector3(pos.x, 0, pos.y) * 0.05f; //移動
+
+        Vector3 diff = transform.position - BefoPos;//移動方向向く
+        diff.y = 0;
+        BefoPos = transform.position;
+        if (diff.magnitude > 0.01f)
+        {
+           transform.rotation = Quaternion.LookRotation(diff);
+        }
+
+        
+
+        if (_actionControl.Player.Jump.triggered && !Jumping)//ジャンプ
+        {
+            //rb.velocity = Vector3.up * upForce;
+            rb.AddForce(new Vector3(0, upForce, 0));
+            Jumping = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            Jumping = false;
+        }
     }
 }
